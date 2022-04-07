e5d78707d1fd449a7b4eed46775381cbe90c1461
hectorcamp <hectorcamp@gmail.com>
2022-04-07 00:55:54 -0300

    Evoluindo as rotas

--- alura-node/src/app.js
+++ alura-node/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
 const livros = [
     {id: 1, "titulo": "Senhor do Aneis"},
     {id: 2, "titulo": "O Hobbit"}
@@ -14,5 +16,28 @@ app.get('/', (req, res) => {
 app.get('/livros', (req, res) => {
     res.status(200).json(livros)
 })
+
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro foi cadastrado com sucesso')
+})
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+
+})
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id)
+}
+
 
 export default app
